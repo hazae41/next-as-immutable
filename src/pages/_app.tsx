5b0bf2b7ef57afe9a0acff7ee62315c64afa5e0a
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-11-12 13:44:30 +0700

    d

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,7 @@ import { useEffect, useRef } from "react";
 async function register() {
   navigator.serviceWorker.addEventListener("controllerchange", () => location.reload())
 
-  const update = process.env.NODE_ENV === "production"
-    ? await Immutable.register("/service_worker.latest.js")
-    : void await navigator.serviceWorker.register("./service_worker.js")
+  const update = await Immutable.register("/service_worker.latest.js")
 
   if (update != null && confirm("Update available, do you want to update now?")) {
     await update()
